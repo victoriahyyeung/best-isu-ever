dc649e04597676fb66174682c9de87b673aa27c3
mikatam-code <bemikatam@gmail.com>
2026-06-02 14:02:40 -0400

    lollll

--- src/Main.java
+++ src/Main.java
@@ -137,6 +137,7 @@ public class Main extends JPanel implements MouseListener, KeyListener {
 				if (i.itemImg != null) {
 					g.drawImage(i.itemImg, i.itemX, i.itemY, this);
 				}
+				System.out.println("Yes");
 			}
 
 		}
