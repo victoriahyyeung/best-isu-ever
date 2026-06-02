7aa72dd154080727024975a1d3a7cfae8a664f40
victoriahyyeung <victoria.hy.yeung@gmail.com>
2026-06-01 23:53:53 -0400

    organized code. blend and cut start time are not needed, using Timer instead. added blendTimer. moved progressbars to main. isReady is removed, can be checked from isBlended (fruit only ready when blended. removed keyPressed, shuld be in main removed currentImage method, can be done in cut() and blend() methods instead. removed draw(), already in Item.

--- src/Fruit.java
+++ src/Fruit.java
@@ -6,55 +6,63 @@ import javax.swing.*;
 public class Fruit extends Item {
 
 	// Images for the 3 fruit states
-	protected Image freshImg;
-    protected Image cutImg;
-    protected Image blendedImg;
-    
-    protected boolean isBlended;
-    protected boolean isCut;
-    
-    protected long blendStartTime;
-	protected long cutStartTime;
-	
-	int chopCount;
-	boolean isReady;
-	
-	JProgressBar blendBar;
-	boolean showBlendBar;
-	
-	JProgressBar cutBar;
-	boolean showCutBar;
-	
-	public Fruit (Image FruitImg) {
-		super (FruitImg);
+	protected Image freshImg, cutImg, blendedImg;
+
+	protected boolean isBlended, isCut;
+
+
+	protected int chopCount;//need 4 chops to become cut
+	protected Timer blendTimer;
+
+	//	JProgressBar blendBar;
+	//	boolean showBlendBar;
+	//	
+	//	JProgressBar cutBar;
+	//	boolean showCutBar;
+	//MOVE TO MAIN!!!
+
+	public Fruit (Image freshImg, Image cutImage, Image blendedImg) {
+		super (freshImg);
+		this.freshImg=freshImg;
+		this.cutImg=cutImg;
+		this.blendedImg=blendedImg;
+		//default fruit states
+		this.isCut=false;
+		this.isBlended=false;
+		this.isCut=false;
+		this.chopCount=0;
+		//capabilities inherited from Item class
+		this.cuttable=true;
+		this.blendable=false;//must be cut first before being blendable
+		this.cookable=false;//never cookable
 	}
-	
-	public void keyPressed(KeyEvent e) {
-		int key = e.getKeyCode();
-		if (key==KeyEvent.VK_SPACE)
-			cut();
-	}
-	
+
 	public void cut() {
-		
+		if (!isCut && !isBlended && cuttable) {//check if cut() can be done
+			chopCount++;//cut
+			if (chopCount>=4) {
+				isCut=true;//will no longer be able to go into this method
+				this.cuttable=false;
+				this.img=cutImg;//change img to cut version
+				this.blendable=true;//NOW can be blended
+			}
+		}
 	}
-	
-	public void blend() {
-		
+
+	public void blend(JProgressBar bar, JPanel panel) {
+		if (!isBlended && blendable) {//check if blend() can even be done
+			isBlended=true;
+			this.img=blendedImg;
+			this.blendable=false;
+		}
 	}
-	
-    public Image currentImage() {
-        if (isBlended) 
-        	return blendedImg;
-        if (isCut) 
-        	return cutImg;
-        return freshImg;
-    }
-    
- 
-    
-    public void draw(Graphics g) {
-        g.drawImage(currentImage(), itemX, itemY, null);
-    }
-    
+
+	//getters
+	public boolean isBlended() {
+		return isBlended;
+	}
+	public boolean isCut() {
+		return isCut;
+	}
+
 }
