4a9fc60008399ad9a71ffc83698903a3482dff58
victoriahyyeung <victoria.hy.yeung@gmail.com>
2026-06-02 00:20:45 -0400

    Pearl class
    progress bar and associated boolean deleted to move to main.
    update cook method.
    added getter for isCooked.

--- src/Pearl.java
+++ src/Pearl.java
@@ -3,19 +3,35 @@ import java.awt.*;
 import javax.swing.JProgressBar;
 public class Pearl extends Item{
 
-	Image cookedPearl;
-	Image uncookedPearl;
+	private Image uncookedImg, cookedImg;
+	private boolean isCooked;
 	
 	JProgressBar cookBar;
 	boolean showCookBar;
 	
 	
 	public Pearl () {
-		super (Toolkit.getDefaultToolkit().getImage("uncookedMango.png"));
+		super (Toolkit.getDefaultToolkit().getImage("pearl_uncooked.png"));
+		this.uncookedImg=this.img;
+		this.cookedImg=Toolkit.getDefaultToolkit().getImage("pearl_cooked.png");
+		this.isCooked=false;
+		//abilities default
+		this.cookable=true;
+		this.cuttable=false;
+		this.blendable=false;
 	}
 	
 	public void cook() {
-		
+		if (!isCooked&& cookable) {//checks if possible
+			isCooked=true;//so that smth cant be cooked more than once
+			this.img=cookedImg;//update image
+			this.cookable=false;//update ability
+		}	
+	}
+	
+	//getter
+	public boolean isCooked() {
+		return isCooked;
 	}
 	
 }
