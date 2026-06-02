062824131db2e12acb66c0b5c77c11edc7edbd97
victoriahyyeung <victoria.hy.yeung@gmail.com>
2026-06-02 00:41:37 -0400

    cup class

--- src/Cup.java
+++ src/Cup.java
@@ -1,29 +1,72 @@
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.util.ArrayList;
 
 public class Cup extends Item {
+	private ArrayList<String> fruits;//(max 2)
+	private ArrayList<String> toppings;//(max 2)
 
-	private ArrayList<String> cupIngredients = new ArrayList <> ();
-	boolean hasMango;
-	boolean hasLychee;
-	boolean hasPearls;
-	
-	Image emptyCup;
-	Image cupWithPearls;
-	Image cupWithMango;
-	Image cupWithLychee;
-	Image cupWithMangoPearls;
-	Image cupWithLycheePearls;
-	
-	boolean isSubmitted;
-	
-	public Cup () {
-		super ((Toolkit.getDefaultToolkit().getImage("cup.png")));
+	private Image baseCupImg;
+	private Image pearlImg;
+	private Image puddingImg;
+
+	public Cup (Image baseCup, Image pearl, Image pudding) {
+		super (baseCup);
+		this.baseCupImg=baseCup;
+		this.pearlImg=pearl;
+		this.puddingImg =pudding;
+		fruits=new ArrayList<>();
+		toppings=new ArrayList<>();
+		//cups cant do any of these actions, its still an item tho.
+		this.cuttable=false;
+		this.blendable=false;
+		this.cookable=false;
 	}
 	
-	public static void main(String[] args) {
-		
+	public boolean addFruit(String fruitType) {
+		if (fruits.size()>=2)//max 2 fruits can be added
+			return false;
+		if (!fruits.contains(fruitType)) {//avoid duplicate fruit
+			fruits.add(fruitType);
+			return true;
+		}
+		return false;
 	}
+	public boolean addTopping (String toppingType) {
+		if (toppings.size()>=2)//max 2 toppings can be added
+			return false;
+		if (!toppings.contains(toppingType)) {
+			toppings.add(toppingType);
+			return true;
+		}
+		return false;
+	}
+	
+	//may be unesccesary
+	public void clear() {
+		fruits.clear();
+		toppings.clear();
+	}
+	
+	public void drawLayered(Graphics g, int x, int y) {
+		g.drawImage(baseCupImg, x, y, null);
+		//TODO: DRAW LIQUID AND TOPPINGS 
+	}
+
+	//getters
+	public ArrayList<String>getFruits(){
+		return fruits;
+	}
+	public ArrayList<String>getToppings(){
+		return toppings;
+	}
+	public boolean hasFruit(String fruit) {
+		return fruits.contains(fruit);
+	}
+	public boolean hasTopping(String topping) {
+		return toppings.contains(topping);
+	}
+	
 
 }
