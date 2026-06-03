da350445ff409c3f5633acda19f93de8fb8b35bb
victoriahyyeung <victoria.hy.yeung@gmail.com>
2026-06-03 01:00:07 -0400

    i fixed main. still needa do drawing the liqud tjho

--- src/Main.java
+++ src/Main.java
@@ -22,15 +22,17 @@ public class Main extends JPanel implements MouseListener, KeyListener, MouseMot
 	private Timer gameTimer;//FOR THE GAME REFRESH REPAINTING
 	private Timer patienceTimer;
 	private JProgressBar actionBar;//progress of cooking/blending
-	
+
 	//stations
 	private Rectangle chopStation=new Rectangle (50, 50, 50, 50);
 	private Rectangle blendStation=new Rectangle (150, 150, 50, 50);
 	private Rectangle cupStation=new Rectangle (250, 250, 50, 50);
 	private Rectangle trayStation=new Rectangle (350, 350, 50, 50);
 
+	private Fruit blendingFruit;
+	private int blendProgress;
+	private Timer blendTimer;
 
-	
 	Image home, instructions1, instructions2, instructions3, instructions4, lockedLevels, unlockedLevels, startImg, gameLevel1, gameLevel2, credits, highScore, victory;
 	// Screen States
 	// 0 - Home
@@ -114,30 +116,31 @@ public class Main extends JPanel implements MouseListener, KeyListener, MouseMot
 		usernameField.setText("user xxxxxxx");
 		usernameField.setForeground(Color.BLACK);
 		usernameField.setVisible(false);
-		
+
 		currentCup=new Cup(Toolkit.getDefaultToolkit().getImage("cup_base.png"), Toolkit.getDefaultToolkit().getImage("pearl_icon.png"), Toolkit.getDefaultToolkit().getImage("pudding_icon.png"));
 		trayDrinks=new ArrayList<>();
 		Image customerImg=Toolkit.getDefaultToolkit().getImage("customer.png");
 		customers.add(new Customer(50, 300, customerImg));
-		
+
 		//bar for blend/cook/cut
 		actionBar= new JProgressBar(0,100);
 		actionBar.setVisible(false);//not visible til action is doing
 		this.add(actionBar);
-		
-gameTimer=new Timer (50, this);
-gameTimer.start();
 
-patienceTimer=new Timer( 1000, this);
-patienceTimer.start();
+		gameTimer=new Timer (50, this);
+		gameTimer.start();
 
-addKeyListener(this);
-setFocusable(true);
+		patienceTimer=new Timer( 1000, this);
+		patienceTimer.start();
+
+		addKeyListener(this);
+		setFocusable(true);
 		this.setLayout(null); // Use absolute positioning for the box
 		this.add(usernameField);
 
-		
-		
+		blendTimer=new Timer(30, this);
+		blendTimer.stop();//not initially running
+
 		try
 		{
 			tracker.waitForAll ();
@@ -145,7 +148,7 @@ setFocusable(true);
 		catch (InterruptedException e){}
 
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource()==gameTimer) {//maybe do dif method?
 			repaint();
@@ -154,6 +157,19 @@ setFocusable(true);
 			for (Customer c: customers) {
 				c.decreasePatience();
 			}
+		}
+		else if (e.getSource()==blendTimer) {
+			blendProgress+=5;
+			if (blendProgress>=100) {
+				blendTimer.stop();
+				actionBar.setVisible(false);;
+				blendingFruit.setBlended();
+				ingredientsOnScreen.add(blendingFruit);
+				blendingFruit=null;
+				repaint();
+			}
+			else
+				actionBar.setValue(blendProgress);
 		}
 	}
 
@@ -320,14 +336,14 @@ setFocusable(true);
 	}
 
 	public void keyPressed(KeyEvent e) {//for some variety ig we do SPACE
-if (e.getKeyCode()==KeyEvent.VK_SPACE)	{
-	if (selectedItem!=null&& selectedItem.type.equals("fruit")) {
-		Fruit f=(Fruit) selectedItem;
-		f.cut();
-		repaint();
+		if (e.getKeyCode()==KeyEvent.VK_SPACE)	{
+			if (selectedItem!=null&& selectedItem.type.equals("fruit")) {
+				Fruit f=(Fruit) selectedItem;
+				f.cut();
+				repaint();
+			}
 		}
 	}
-}
 
 
 	public static void main(String[] args) {
@@ -360,7 +376,7 @@ if (e.getKeyCode()==KeyEvent.VK_SPACE)	{
 		x = e.getX();
 		y = e.getY();
 		handleAction(x, y);
-		
+
 		for (int i = ingredientsOnScreen.size() - 1; i >= 0; i--) {
 			Item item = ingredientsOnScreen.get(i);
 			if (item.contains(e.getX(), e.getY())) {
@@ -378,10 +394,90 @@ if (e.getKeyCode()==KeyEvent.VK_SPACE)	{
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		selectedItem = null;
+		if (selectedItem==null)
+			return;
+		int mx=e.getX();
+		int my=e.getY();
+		String type=selectedItem.type;
+		if (type.equals("fruit")) {
+			Fruit f=(Fruit) selectedItem;
+			//chopboard
+			if (chopStation.contains(mx,my)) {
+				ingredientsOnScreen.add(f);
+			}
+			//blender
+			else if (blendStation.contains(mx,my)) {
+				if (f.isCut()&& !f.isBlended()) {
+					startBlendingAnimation(f);
+				}
+				else {
+					JOptionPane.showMessageDialog(this, "Chop the fruit first!");
+					ingredientsOnScreen.add(f);
+				}
+			}
+			//cup station (add components to cup)
+			else if (cupStation.contains (mx,my)) {
+				if (f.isBlended()) {
+					if (currentCup==null) {
+						Image cupBase=Toolkit.getDefaultToolkit().getImage("cup_base.png");
+						Image pearlIcon=Toolkit.getDefaultToolkit().getImage("pearl_icon.png");
+						Image puddingIcon=Toolkit.getDefaultToolkit().getImage("pudding_icon.png");
+						currentCup=new Cup (cupBase,pearlIcon,puddingIcon);
+					}
+					currentCup.addFruit(f.getFruitType());
+				}else {
+					JOptionPane.showMessageDialog(this, "Blend the fruit first!!!!");
+					ingredientsOnScreen.add(f);
+				}
+			}
+			else {
+				ingredientsOnScreen.add(f);
+			}
+		}
+		else if(type.equals("pearl")) {
+			Pearl p=(Pearl) selectedItem;
+			if (cupStation.contains(mx,my)) {
+				if (currentCup==null) {
+					Image cupBase = Toolkit.getDefaultToolkit().getImage("cup_base.png");
+					Image pearlIcon = Toolkit.getDefaultToolkit().getImage("pearl_icon.png");
+					Image puddingIcon = Toolkit.getDefaultToolkit().getImage("pudding_icon.png");	
+					currentCup=new Cup(cupBase,pearlIcon,puddingIcon);
+				}
+				currentCup.addTopping("pearls");//good
+			}
+			else {
+				ingredientsOnScreen.add(p);
+			}
+		}
+		else if (type.equals("cup")) {
+			Cup cup=(Cup) selectedItem;
+			if (trayStation.contains(mx,my)) {
+				if (!cup.getFruits().isEmpty()|| !cup.getToppings().isEmpty()) {
+					trayDrinks.add(cupToDrink(cup));
+					Image cupBase = Toolkit.getDefaultToolkit().getImage("cup_base.png");
+					Image pearlIcon = Toolkit.getDefaultToolkit().getImage("pearl_icon.png");
+					Image puddingIcon = Toolkit.getDefaultToolkit().getImage("pudding_icon.png");
+
+					currentCup=new Cup(cupBase,pearlIcon,puddingIcon);
+					JOptionPane.showMessageDialog(this, "Drink added to tray.");;
+
+				}
+				else {
+					JOptionPane.showMessageDialog(this, "empty cup...");
+					currentCup=cup;
+				}
+			}
+			else
+				currentCup=cup;
+		}
+
+		selectedItem=null;
+		repaint();
 
 	}
-
+	private Drink cupToDrink(Cup cup) {
+		return new Drink(cup.getFruits(), cup.getToppings());
+	}
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
@@ -409,5 +505,14 @@ if (e.getKeyCode()==KeyEvent.VK_SPACE)	{
 		// TODO Auto-generated method stub
 
 	}
+
+private void startBlendingAnimation(Fruit f) {
+	blendingFruit=f;
+	blendProgress=0;
+	actionBar.setValue(0);
+	actionBar.setVisible(true);
+	blendTimer.start();
+}
+
 
 }
