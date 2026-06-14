2c36c83b5b3ad30c4c475d33866cb4c37492d693
victoriahyyeung <victoria.hy.yeung@gmail.com>
2026-06-14 01:13:37 -0400

    Merge branch 'main' of https://github.com/victoriahyyeung/best-isu-ever.git into main

--- src/Drink.java
+++ src/Drink.java
@@ -1,28 +1,42 @@
 import java.util.*;
 public class Drink {
-private ArrayList<String>fruits;
-private ArrayList<String> toppings;
- public Drink(ArrayList<String> fruits, ArrayList<String> toppings) {
-	 this.fruits=new ArrayList<>(fruits);
-	 this.toppings=new ArrayList<>( toppings);
- }
- 
- 
- //equals
- public boolean equals(Object o) {
-	 Drink d = (Drink) o;
-	 if (d==null)
-		 return false;
-	 return this.fruits.containsAll(d.fruits) && d.fruits.containsAll(this.fruits) && this.toppings.containsAll(d.toppings) && d.toppings.containsAll(this.toppings);
- }
- 
- 
- //getters
- public ArrayList<String> getFruits(){
-	 return fruits;
- }
- public ArrayList<String> getToppings(){
-	 return toppings;
- }
+	private ArrayList<String>fruits;
+	private ArrayList<String> toppings;
+
+
+	//Description: Constructor for Drink class, creates a drink with specified fruits and toppings
+	// Parameters: list of fruit types in the drink, list of topping types in the drink
+	// Return: none
+	public Drink(ArrayList<String> fruits, ArrayList<String> toppings) {
+		this.fruits=new ArrayList<>(fruits);
+		this.toppings=new ArrayList<>( toppings);
+	}
+
+
+	//Description: Compares this drink to another object for equality based on fruits and toppings (order independent)
+	// Parameters:  the object to compare against
+	// Return: true if drinks have same fruits and toppings (order doesn't matter), false otherwise
+	public boolean equals(Object o) {
+		Drink d = (Drink) o;
+		if (d==null)
+			return false;
+		return this.fruits.containsAll(d.fruits) && d.fruits.containsAll(this.fruits) && this.toppings.containsAll(d.toppings) && d.toppings.containsAll(this.toppings);
+	}
+
+
+	//getters
+	//Description: Returns the list of fruits in this drink
+	// Parameters: None
+	// Return: list of fruit types
+	public ArrayList<String> getFruits(){
+		return fruits;
+	}
+
+	//Description: Returns the list of toppings in this drink
+	// Parameters: None
+	// Return:  list of topping types
+	public ArrayList<String> getToppings(){
+		return toppings;
+	}
 
 }
--- src/Item.java
+++ src/Item.java
@@ -4,82 +4,119 @@ import java.awt.event.MouseEvent;
 import java.awt.*;
 
 abstract class Item {
-    
-    protected int x, y, width, height;
-    protected Image img;
-    protected boolean isHeld; //true when being dragged
-    
-    protected boolean blendable;
-    protected boolean cuttable;
-    protected boolean cookable;
-    protected String type;//fruit, pearl, cup, etc
-    
-   
-   protected boolean isFruit;
-   protected boolean isTopping;
-    
-    
-    public Item (Image img, String type) {
-    	this.type = type;
-    	this.img = img;
-    	if (img!=null) {//getting width and height
-    		this.width=img.getWidth(null);
-    		this.height=img.getHeight(null);
-    	}
-    	else
-    		System.out.print("ITEM IMAGE IS NULL!!!!");
-    	//default, subclasses must override
-    	this.blendable=false;
-    	this.cuttable=false;
-    	this.cookable=false;
-    }
-    
-    public void setPosition(int newX, int newY) {
-    	this.x = newX;
-        this.y = newY;
-    }
-    
-    public int getX() {
-    	return this.x;
-    }
-    
-    public int getY() {
-    	return this.y;
-    }
-    public boolean isFruit() {
-    	return isFruit;
-    }
-    
-    public boolean isTopping() {
-    	return isTopping;
-    }
-    
-    public boolean isBlendable() {
-    	return blendable;
-    }
-    
-    public boolean isCuttable() {
-    	return cuttable;
-    }
-    
-    public boolean isCookable() {
-    	return cookable;
-    }
-    
-    public Rectangle getBounds() {
-        return new Rectangle(x, y, width, height);
-    }
-    //simplifies the main
-    public boolean contains (int mx, int my) {
-    	return getBounds().contains(mx, my);
-    }
-    //for organizion and convenience when drawing
-    public void draw(Graphics g) {
-    	if (img!=null) {
-    		g.drawImage(img,  x, y, null);
-    	}
-    	
-    }
-    
-    
+
+	// item position and dimensions
+	protected int x, y, width, height;
+	protected Image img; //item image
+	protected boolean isHeld; //true when being dragged
+
+	// whether item is blendable/cuttable/cookable depending on item nature
+	protected boolean blendable;
+	protected boolean cuttable;
+	protected boolean cookable;
+	protected String type;//fruit, pearl, cup, etc
+
+
+	// categorizes item as fruit or topping
+	protected boolean isFruit;
+	protected boolean isTopping;
+
+	// Description: Constructor for Item class, initializes an item with an image and type
+	// Parameters:the image to display for this item,  the type of item (fruit, pearl, cup, etc.)
+	// Return: none
+	public Item (Image img, String type) {
+		this.type = type;
+		this.img = img;
+		if (img!=null) {//getting width and height
+			this.width=img.getWidth(null);
+			this.height=img.getHeight(null);
+		}
+		else
+			System.out.print("ITEM IMAGE IS NULL!!!!");
+		//default, subclasses must override
+		this.blendable=false;
+		this.cuttable=false;
+		this.cookable=false;
+	}
+
+	// Description: Sets the position of the item on screen
+	// Parameters: int newX - new x coordinate, int newY - new y coordinate
+	// Return: void
+	public void setPosition(int newX, int newY) {
+		this.x = newX;
+		this.y = newY;
+	}
+
+	// Description: Returns the x coordinate of the item
+    // Parameters: None
+    // Return: x position
+	public int getX() {
+		return this.x;
+	}
+
+	 // Description: Returns the y coordinate of the item
+    // Parameters: None
+    // Return: y position
+	public int getY() {
+		return this.y;
+	}
+	
+	// Description: Returns whether this item is a fruit
+    // Parameters: None
+    // Return: true if item is fruit, false otherwise
+	public boolean isFruit() {
+		return isFruit;
+	}
+
+	// Description: Returns whether this item is a topping
+    // Parameters: None
+    // Return:true if item is topping, false otherwise
+	public boolean isTopping() {
+		return isTopping;
+	}
+
+	// Description: Returns whether this item can be blended
+    // Parameters: None
+    // Return: true if blendable, false otherwise
+	public boolean isBlendable() {
+		return blendable;
+	}
+
+	 // Description: Returns whether this item can be cut
+    // Parameters: None
+    // Return: true if cuttable, false otherwise
+	public boolean isCuttable() {
+		return cuttable;
+	}
+
+	 // Description: Returns whether this item can be cooked
+    // Parameters: None
+    // Return:true if cookable, false otherwise
+	public boolean isCookable() {
+		return cookable;
+	}
+
+	 // Description: Returns the bounding rectangle of the item for collision detection
+    // Parameters: None
+    // Return: the bounding rectangle
+	public Rectangle getBounds() {
+		return new Rectangle(x, y, width, height);
+	}
+	 // Description: Checks if a point (mouse click) is within the item's boundaries
+    // Parameters:  mouse x coordinate, mouse y coordinate
+    // Return: true if point is inside item, false otherwise
+	public boolean contains (int mx, int my) {
+		return getBounds().contains(mx, my);
+	}
+	// Description: Draws the item's image at its current position
+    // Parameters: graphics object for drawing
+    // Return: void
+	public void draw(Graphics g) {
+		if (img!=null) {
+			g.drawImage(img,  x, y, null);
+		}
+
+	}
+
+
 }
